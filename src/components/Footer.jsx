7cd50a18d62bf1962cbafe32e2a0dbfeaf6c1995
udodi-n <udodiworkandfreelance@gmail.com>
2025-12-26 15:31:03 +0100

    responseFix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
     return(
-        <footer className="w-full flex flex-col text-center text-white py-2"
+        <footer className="w-full flex flex-col mt-8 text-center text-white py-2"
         style={{fontSize: "calc(8px + 0.5vw)"}}>
             <a href="/terms" className="underline">Terms & Conditions</a>
             <p>© Copyright 2025 <a href="https://x.com/ryoichi_xl" className="underline text-[#ee2d2e]" target="_blank">Ryoichi.</a> <br /> Alle Rechte vorbehalten.</p>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,8 +6,8 @@ function Hero() {
         <div className="h-screen flex flex-col justify-center items-center text-center gap-16">
             <div className="transform rotate-5 bg-white flex justify-center p-3 lg:hidden"
             style={{
-                width: "calc(15rem + 2vw)", 
-                height: "calc(15rem + 2vw)",
+                width: "calc(12rem + 2vw)", 
+                height: "calc(12rem + 2vw)",
                 boxShadow: "3px 3px 16px #ffffff1a"
             }}>
                 <div className="w-full h-4/5 bg-black">
@@ -15,10 +15,10 @@ function Hero() {
                 </div>
             </div>
             <h1 className=""
-            style={{fontSize: "clamp(45px, 4vw, 80px)",
+            style={{fontSize: "clamp(40px, 4vw, 75px)",
             lineHeight: "clamp(40px, 4vw, 80px)"
             }}><span className="font-[Euro] text-[#ee2d2e]"
-            style={{fontSize: "calc(80px + 1vw)"}}>MORALS</span> <br /> <span className="relative z-2">ARE ONLY FOR <br />PEOPLE WHO <br /> GET </span><span className="bg-[#ee2d2e] relative z-1 transform rotate-20">CAUGHT</span></h1>
+            style={{fontSize: "calc(75px + 0.5vw)"}}>MORALS</span> <br /> <span className="relative z-2">ARE ONLY FOR <br />PEOPLE WHO <br /> GET </span><span className="bg-[#ee2d2e] relative z-1 transform rotate-20">CAUGHT</span></h1>
 
             <div style={{width: "calc(13rem + 0.5vw)"}}>
                 <Button nav="/sign-up" text="HOP IN"/></div>
