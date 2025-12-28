f60bb401475f6876d8a51471c238e8f649ecb46e
udodi-n <udodiworkandfreelance@gmail.com>
2025-12-28 08:25:14 +0100

    serverLess

--- src/components/Gif.jsx
+++ src/components/Gif.jsx
@@ -4,18 +4,17 @@ import { auth } from '../firebase'
 import { onAuthStateChanged } from 'firebase/auth'
 
 function Gif({display, setDisplay}) {
-    if (!display) return null
     const [value, setValue] = useState('')
     const [uid, setUid] = useState('')
     const [gif, setGif] = useState([])
+
+        
     
     async function searchGif() {
         if(uid) {
             const res = await fetch(`/api/gifs?q=${value}&uid=${uid}`);
-
             const data = await res.json();
-            const urls = data.data.data.map(gif => gif.file.sm.gif);
-            setGif(urls)
+            setGif(data.urls)
             console.log(urls)
         }
     }
@@ -27,12 +26,24 @@ function Gif({display, setDisplay}) {
         })
     }, [])
 
+    useEffect(() => {
+        if (!uid || !value) return;
+
+        const timeout = setTimeout(() => {
+            searchGif();
+        }, 400);
+
+        return () => clearTimeout(timeout);
+        }, [value, uid]);
+
+        if (!display) return null
+        
     return(
         <div className={`flex flex-col items-center absolute z-100 h-screen bg-white w-full p-3`}>
         <div className="h-10 w-full flex justify-around items-center ">
             <textarea
                     placeholder="GIF"
-                    onChange={(e) => {searchGif(); setValue(e.target.value)}}
+                    onChange={(e) => setValue(e.target.value)}
                     className=" border-b-1 border-black text-black/80 h-full overflow-x-auto focus:outline-none whitespace-nowrap p-3"
                     value={value}
                     style={{
@@ -42,13 +53,13 @@ function Gif({display, setDisplay}) {
                 <img onClick={() => setDisplay(false)} src={cancel} className="w-5 h-auto" alt="" /> 
         </div>
         <div className="w-full flex flex-1 flex-wrap gap-2 h-[80vh] overflow-y-auto">
-        {gif.map((giphy, i) => (
+        {gif.map((url, i) => (
             <div
             key={i}
             className="relative w-[calc(33.333%-0.5rem)] sm:w-[calc(25%-0.5rem)] md:w-[calc(20%-0.5rem)] lg:w-[calc(16.666%-0.5rem)] aspect-square overflow-hidden rounded"
             >
             <img
-                src={giphy.url}
+                src={url}
                 alt=""
                 className="w-full h-full object-cover"
             />
